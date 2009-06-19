070862efac97d9e93af3170faafbd409b8e9283b
sgbeal <sgbeal@dd8145e8-5ab0-11de-b31f-81d54249e7d9>
2009-06-19 16:11:09 +0000

    egg

---
+++ trunk/makeDist.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+########################################################################
+# A quick hack to generate a source dist file for QBoard.
+
+VERSION=$(date +%Y%m%d)
+
+DEST=libwhefs-${VERSION}
+echo "Creating $DEST ..."
+
+
+test -d ${DEST} && rm -fr ${DEST}
+mkdir ${DEST}
+test -d ${DEST} || {
+    echo "Destination dir [${DEST}] could not be created!"
+    exit 1
+}
+find . -name '*~' | xargs rm -f
+
+sdirs="src include doc app"
+
+find doc -type d -name 'libwhefs-API*' | xargs rm -fr
+
+echo "Copying files..."
+for d in $sdirs; do
+    echo -e "\t... $d"
+    find $d \
+	-type f \
+	-name '*.h' \
+	-o -name '*.c' \
+	-o -name 'Makefile' \
+	-o -name 'Doxyfile' \
+	-o -name 'Doxyfile-internal' \
+	| sed -e '/\/bak/d' \
+	-e '/\/nono/d' \
+	-e '/\.#/d' \
+	-e '/\.svn/d' \
+	> filelist
+    cp --parents $(cat filelist) $DEST || {
+	err=$?
+	echo "Copy failed!"
+	rm filelist
+	exit $err
+    }
+    rm filelist
+
+    #echo cp --parents $(set -x; find $d $nameargs)
+done
+
+cp LICENSE \
+    README \
+    Makefile \
+    config.make \
+    common.make \
+    $DEST
+
+echo "Kludging in static version number..."
+
+find $DEST -name nono -o -name release -o -name debug | xargs rm -fr
+find $DEST -name  debug -o -name release | xargs rm -fr
+
+echo "Tarring...";
+TF=$DEST.tar.bz2
+tar cjf $TF $DEST
+echo "Cleaning up..."
+rm -fr $DEST
+
+echo "Done:"
+ls -l $TF
