1c18ea3e549322d2a6778d5383d785c1fed2e0b3
sgbeal <sgbeal@dd8145e8-5ab0-11de-b31f-81d54249e7d9>
2009-07-01 19:25:49 +0000

    Minor tweaks.

--- trunk/app/plus.cpp
+++ trunk/app/plus.cpp
@@ -11,26 +11,22 @@ int main( int argc, char const ** argv )
     whio_stream * wout = whio_stream_for_filename( "/dev/stdout", "w+" );
     assert( wout );
 
-    {
-        std::ostringstream dummy;
-        whio::ostreambuf_whio os( dummy, wout, false );
-        dummy << "Hi, world!\n";
-    }
+    std::ostringstream sout;
+    whio::ostreambuf_whio os( sout, wout, true );
+    sout << "Hi, world!\n";
     wout->api->flush(wout);
 
     whio_stream * win = whio_stream_for_filename( "/dev/stdin", "r" );
     assert( win );
     {
         std::istringstream din;
-        whio::istreambuf_whio os( din, win, false );
+        whio::istreambuf_whio os( din, win, true );
         MARKER("Enter an INTEGER and tap ENTER, then CTRL-D: ");
         fflush(stdout);
         int x = -1;
         din >> x;
         MARKER("read int=%d\n",x);
     }
-    win->api->finalize(win);
-    wout->api->finalize(wout);
 
     return 0;
 }
