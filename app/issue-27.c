360b0697eedd808ae08e7502ca5a69e966d40921
sgbeal <sgbeal@dd8145e8-5ab0-11de-b31f-81d54249e7d9>
2011-09-27 14:26:37 +0000

    Narrowed issue #27 bug down to only 1 of the 6 possible closing order combinations (but why it is happening, and why only in that combination, is still a riddle).

--- app/issue-27.c
+++ app/issue-27.c
@@ -26,19 +26,39 @@ void test_rw3files_2(){
     whefs_fs *fs=NULL;
     int errCode = whefs_mkfs_dev(dev, &options, &fs, true);
     assert(0 == errCode);
-    whefs_file* rs1 = whefs_fopen(fs, "rs1", "r+");
-    assert(NULL != rs1);
-    whefs_file* rs2 = whefs_fopen(fs, "rs2", "r+");
-    assert(NULL != rs2);
-    whefs_file* rs3 = whefs_fopen(fs, "rs3", "r+");
-    assert(NULL != rs3);
+    whefs_file* rs1;
+    whefs_file* rs2;
+    whefs_file* rs3;
 
-    errCode = whefs_fclose(rs2);
-    assert(0 == errCode);
-    errCode = whefs_fclose(rs3);//crash here
-    assert(0 == errCode);
-    errCode = whefs_fclose(rs1);
-    assert(0 == errCode);
+    rs1 = rs2 = rs3 = NULL;
+
+#define CL(F) errCode = whefs_fclose(F); assert(0==errCode)
+    /* valgrind is revealing lib-level memory mis-use
+       in some whefs_fclose() ordering combinations...
+
+       These closing orderings seem to work:
+
+       (2,1,3)
+       (1,2,3)
+       (1,3,2)
+       (3,2,1)
+       (3,1,2)
+
+       These break:
+
+       (2,3,1)
+
+       weird!
+    */
+    rs1 = whefs_fopen(fs, "rs1", "r+");
+    assert(NULL != rs1);
+    rs2 = whefs_fopen(fs, "rs2", "r+");
+    assert(NULL != rs2);
+    rs3 = whefs_fopen(fs, "rs3", "r+");
+    assert(NULL != rs3);
+    CL(rs2);
+    CL(rs3);
+    CL(rs1);
     whefs_fs_finalize(fs);
     free(buf);
 }
@@ -47,5 +67,6 @@ int main( int argc, char const ** argv )
 {
     test_rw3files_2();
     puts("If you got this far without crashing, then could not reproduce problem.");
+    puts("BUT BE SURE TO RUN IT THROUGH VALGRIND to check for invalid mem access.");
     return 0;
 }
